6b7b7533fd861b53f91d9184eea712ac17c50376
botiuk <svetabotiuk@gmail.com>
2025-02-12 13:44:13 +0200

    changed main controller and header

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,7 +4,11 @@ class MainController < ApplicationController
   skip_before_action :authenticate_user!
   authorize_resource class: false
 
-  def index; end
+  def index
+    last_product_ids = Storehouse.where(operation_type: 'input').order(updated_at: :desc).limit(12).pluck(:product_id)
+    @products = Product.where(id: last_product_ids)
+    avaliable_products_hash
+  end
 
   def contacts; end
 end
