082d25d718b43356b5065ae5d8dd7721c71047f5
botiuk <svetabotiuk@gmail.com>
2025-03-05 14:20:11 +0200

    fixed error from CI workflow

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class MainController < ApplicationController
-  skip_before_action :authenticate_user!, except: :user_profile
+  skip_before_action :authenticate_user!, only: %i[index contacts]
   authorize_resource class: false
 
   def index
